4d1cab609ec65ba783c772546117c633b9ccca9b
Nikolay Neykov <nikolayneykov92@gmail.com>
2021-02-11 23:34:18 +0200

    added 05.numbers-from-m-to-n.js

---
+++ soft-uni/01.basic-syntax-conditional-statements-and-loops-lab/05.numbers-from-m-to-n.js
@@ -0,0 +1,15 @@
+// function getNumbersInRange(m, n) {
+//   const numbers = [];
+
+//   for (let i = m; i >= n; i--) {
+//     numbers.push(i);
+//   }
+
+//   return numbers.join("\n");
+// }
+
+function getNumbersInRange(m, n) {
+  return Array.from({ length: m - n + 1 }, (_, i) => m - i).join("\n");
+}
+
+console.log(getNumbersInRange(6, 2));
