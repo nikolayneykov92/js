906ee41870fa8f9ad053a86b351808c99503c4b1
Teodora Zaharieva <teodora.zaharieva.a20@learn.telerikacademy.com>
2021-02-13 01:17:26 +0200

    added 03.word-anagrams.js

---
+++ telerik/mock-exam-2/03.word-anagrams.js
@@ -0,0 +1,18 @@
+const input = ["anagram", "6", "gramana", "aaagrnm", "anagra", "margana", "abc", "xy"]
+const print = this.print || console.log
+const gets = this.gets || ((i) => () => input[i++])(0)
+
+const word = gets().split('')
+const n = +gets();
+
+for(let i = 0; i < n; i++) {
+  const currentWord = gets().split('') 
+  if(word === currentWord) {
+    print('Yes')
+  } else {
+    print('No')
+  }
+}
+
+n
+word
