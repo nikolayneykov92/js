04d8363ce2e9809993d6204c7a49c6fc5ab03e51
Nikolay Neykov <nikolayneykov92@gmail.com>
2021-02-17 21:16:07 +0200

    added 07.triangle-of-numbers.js

--- soft-uni/02.basic-syntax-conditional-statements-and-loops-exercise/07.triangle-of-numbers.js
+++ soft-uni/02.basic-syntax-conditional-statements-and-loops-exercise/07.triangle-of-numbers.js
@@ -1,17 +1,21 @@
+// function printTriangleOfNumbers(n) {
+//   let triangle = ''
+
+//   for (let row = 1; row <= n; row++) {
+//     let currentLine = ''
+
+//     for (let col = 1; col <= row; col++) {
+//       currentLine += row + ' '
+//     }
+
+//     triangle += currentLine + '\n'
+//   }
+
+//   return triangle
+// }
+
 function printTriangleOfNumbers(n) {
-  let triangle = ''
-
-  for (let row = 1; row <= n; row++) {
-    let currentLine = ''
-
-    for (let col = 1; col <= row; col++) {
-      currentLine += row + ' '
-    }
-
-    triangle += currentLine + '\n'
-  }
-
-  return triangle
+  return Array.from({ length: n }, (_, row) => Array.from({ length: row + 1 }, () => row + 1).join(' ')).join('\n')
 }
 
 console.log(printTriangleOfNumbers(5))
