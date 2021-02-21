b50d5b35b80ecad43ecc71c38bf80362d6c8e1f7
Nikolay Neykov <nikolayneykov92@gmail.com>
2021-02-21 22:00:59 +0200

    added more solutions for reverse-string.js

--- soft-uni/03.basic-syntax-conditional-statements-and-loops-more-exercise/04.reverse-string.js
+++ soft-uni/03.basic-syntax-conditional-statements-and-loops-more-exercise/04.reverse-string.js
@@ -1,5 +1,78 @@
+// function reverseString(string) {
+//   return string.split('').reverse().join('')
+// }
+
+// function reverseString(string) {
+//   let reversedString = ''
+
+//   for (let i = string.length - 1; i >= 0; i--) {
+//     reversedString += string[i]
+//   }
+
+//   return reversedString
+// }
+
+// function reverseString(string) {
+//   let reversedString = ''
+
+//   for (let i = 0; i < string.length; i++) {
+//     reversedString = string[i] + reversedString
+//   }
+
+//   return reversedString
+// }
+
+// function reverseString(string) {
+//   let reversedString = ''
+
+//   for (let i = 0; i < string.length; i++) {
+//     reversedString += string[string.length - 1 - i]
+//   }
+
+//   return reversedString
+// }
+
+// function reverseString(string) {
+//   let reversedElements = string.split('')
+
+//   for (let i = 0; i < reversedElements.length / 2; i++) {
+//     const leftIndex = i
+//     const rightIndex = reversedElements.length - 1 - i
+//     const leftElement = reversedElements[leftIndex]
+//     const rightElement = reversedElements[rightIndex]
+
+//     reversedElements[leftIndex] = rightElement
+//     reversedElements[rightIndex] = leftElement
+//   }
+
+//   return reversedElements.join('')
+// }
+
+// function reverseString(string) {
+//   let reversedElements = string.split('')
+//   let leftIndex = 0
+//   let rightIndex = string.length - 1
+
+//   while (leftIndex < rightIndex) {
+//     const leftElement = reversedElements[leftIndex]
+//     const rightElement = reversedElements[rightIndex]
+
+//     reversedElements[leftIndex] = rightElement
+//     reversedElements[rightIndex] = leftElement
+
+//     leftIndex++
+//     rightIndex--
+//   }
+
+//   return reversedElements.join('')
+// }
+
+// function reverseString(string) {
+//   return string.split('').map((_, index, array) => array[array.length - 1 - index], '').join('')
+// }
+
 function reverseString(string) {
-  return string.split('').reverse().join('')
+  return string.split('').reduce((reversedString, char) => char + reversedString, '')
 }
 
-console.log(reverseString('SoftUni'))
+console.log(reverseString('12345'))
